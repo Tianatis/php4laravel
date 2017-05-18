bfb8240d1af0c22c610cbc9a71a47b230e5226ae
Tianatis <lin321@mail.ru>
2017-05-18 13:58:58 +0300

    Add templates, styles& images

---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<article class="post">
+	<header class="entry-header">
+		<h1 class="entry-title">{{ $title }}</h1>
+	</header>
+	<!-- .entry-header -->
+
+	<div class="entry-content">
+		<p>Этот блог создан в рамках курса PHP Strong</p>
+	</div>
+	<!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<div class="clear">
+			<div class="ingrid-social-share">
+				<div class="share-links">
+								
+				</div>
+			</div>
+		</div>
+		</footer>
+	<!-- .entry-footer -->
+</article>
